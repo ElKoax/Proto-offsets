85468502f793aa180e4f7123c5a2f34fd70327d6
DiscordBotterGay <55136159+DiscordBotterGay@users.noreply.github.com>
2021-04-27 07:29:12 +0100

    Updated for 27/04/2021
    Yes

---
+++ protos.hpp
@@ -0,0 +1,31 @@
+// Proto namespace and Struct By ElKoax 2021.
+ 
+// These will be updated. every update. 
+
+namespace PProto
+{
+    struct rProto
+    {
+	public:
+		int p_sizecode = 68;
+		int p_sizep = 60;
+		int p_sizek = 76;
+		int p_lineinfo_comp = 56;
+		int p_sizelineinfo = 72;
+		int p_sizelocvars = 64;
+		int p_sizeupvalues = 52;
+		int p_source = 12;
+		int p_maxstacksize = 82;
+		int p_nups = 80; // proto->nups (Number of Up Values)
+		int p_numparams = 83;
+		int is_vararg = 81;
+		/* ^  Non Obfuscated proto memeber's thx roblox for trying to protect them somewhat*/
+		int p_upvalues = 20;
+		int p_locvars = 28;
+		int p_code = 24;
+		int p_k = 8;
+		int p_p = 36; // LOL pp
+		int p_lineinfo = 16;
+	
+    };
+}
