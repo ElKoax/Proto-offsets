f7dacdc346f54b9c1b9501212a5d2c09fe8a042b
ElKoax <47944778+ElKoax@users.noreply.github.com>
2021-04-29 21:50:40 +0100

    Updated for: version-0658018801724832
    Should all work fully, i will be adding proto Deobfuscation and Proto Obfuscation pretty soon

--- protos.hpp
+++ protos.hpp
@@ -7,24 +7,24 @@ namespace PProto
     struct rProto
     {
 	public:
-		int p_sizecode = 68;
-		int p_sizep = 60;
-		int p_sizek = 76;
-		int p_lineinfo_comp = 56;
-		int p_sizelineinfo = 72;
-		int p_sizelocvars = 64;
-		int p_sizeupvalues = 52;
-		int p_source = 12;
+		int p_sizecode = 56;
+		int p_sizep = 64;
+		int p_sizek =  68;
+		int p_lineinfo_comp = 72;
+		int p_sizelineinfo = 52;
+		int p_sizelocvars = 76;
+		int p_sizeupvalues = 60;
+		int p_source = 20;
 		int p_maxstacksize = 82;
 		int p_nups = 80; // proto->nups (Number of Up Values)
 		int p_numparams = 83;
 		int is_vararg = 81;
 		/* ^  Non Obfuscated proto memeber's thx roblox for trying to protect them somewhat*/
-		int p_upvalues = 20;
-		int p_locvars = 28;
-		int p_code = 24;
-		int p_k = 8;
-		int p_p = 36; // LOL pp
+		int p_upvalues = 24;
+		int p_locvars = 32;
+		int p_code = 12;
+		int p_k = 36;
+		int p_p = 8; // LOL pp
 		int p_lineinfo = 16;
 	
     };
